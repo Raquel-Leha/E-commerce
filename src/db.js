5cffd8c55604f4b40416bd35debc3fc30d8c771b
Raquel_Leha <rlopezn833@gmail.com>
2023-09-15 11:26:14 +0200

    setUp

---
+++ src/db.js
@@ -0,0 +1,11 @@
+
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://rlopezn833:los3pilares@cluster0.khyodqw.mongodb.net/commerce');
+    console.log('Base de datos conectada');
+  } catch (error) {
+    console.log(error);
+  }
+};
